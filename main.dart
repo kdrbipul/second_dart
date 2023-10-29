58fdabe6d80e326a2e98fdcc63a18bb931bc196f
kdrbipul <kdrbipul@gmail.com>
2023-10-30 00:30:16 +0600

    second commit

--- main.dart
+++ main.dart
@@ -21,4 +21,9 @@ var firstName = 'md abdul';
 print(firstname); //firstname ar সাথে  variable এর নাম মেলে নাই কারণ আমারা 
 
 
+
+//   প্রতিটা statement এর শেষে সেমিকোলন দেয়া বাধ্যতামূলক যেমন
+var comment = ('আমাদের দেশের নাম বাংলাদেশ');  /*এখানে আমরা comment নামের variable এর মধ্যে যে value টা দিয়েছি তারপর একটা সেমিকোলন দিয়েছি।
+এটাই হলো মূলত সেমিকোলন। অর্থাৎ প্রতিটিা statement এর শেষে সেমিকোলন দেয়াটা বাধ্যতা মূলক।  */
+print(comment);
 }
