2b7161a568eb45624f5fdbb2306957186f1602b7
kdrbipul <kdrbipul@gmail.com>
2023-10-29 22:37:44 +0600

    second dart

---
+++ main.dart
@@ -0,0 +1,24 @@
+void main(){
+  // white space
+  var name = 'abdul    kader';
+  print(name);
+
+
+
+  // line spacing
+  var x =
+      'abdul kader';
+
+
+
+
+  print(x);
+
+
+
+//  case sensitive
+var firstName = 'md abdul';
+print(firstname); //firstname ar সাথে  variable এর নাম মেলে নাই কারণ আমারা 
+
+
+}
